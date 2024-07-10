494d9a1e33eea6c12f11329271f971f65bba8548
Rao Abrar Nawaz <fa23-rcs-014@cuilahore.edu.pk>
2024-07-10 19:53:37 +0500

    added model and admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Home)
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    extra = 1
+      
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    inlines = [
+        
+        ProfileInline,
+    ]
+    
+class SkillsInline(admin.TabularInline):
+    model = Skills
+    extra = 2
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        SkillsInline,
+    ]
+    
+    
+admin.site.register(Portfolio)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+# Home model
+
+class Home(models.Model):
+    name = models.CharField(max_length=20)
+    greetings_1 = models.CharField(max_length=10)
+    greetings_2 = models.CharField(max_length=10)
+    picture = models.ImageField(upload_to='picture/', height_field=None, width_field=None, max_length=None)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+# About model
+class About(models.Model):
+    heading = models.CharField(max_length = 150)
+    career = models.CharField(max_length = 150)
+    description = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to='profile/', height_field=None, width_field=None, max_length=None)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.career
+
+class Profile(models.Model):
+    about = models.ForeignKey(About, on_delete=models.CASCADE)
+    social_name = models.CharField(max_length = 150)
+    link = models.URLField(max_length=200)
+    
+    
+
+class Category(models.Model):
+    name  = models.CharField( max_length=50)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Skill'
+        verbose_name_plural = 'Skills'
+        
+    def __str__(self):
+        return self.name   
+    
+class Skills(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    skill_name = models.CharField(max_length=50)
+    
+    
+    
+class Portfolio(models.Model):
+    image = models.ImageField(upload_to='portfolio/', height_field=None, width_field=None, max_length=None)
+    link = models.URLField(max_length=200)
+    
+    
+    def __str__(self):
+        return f'Portfolio {self.id}'
+    
+    
