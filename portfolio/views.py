bb7e1e8ed6e21bc49e96e1d1313915362dde86f7
Rao Abrar Nawaz <fa23-rcs-014@cuilahore.edu.pk>
2024-07-11 10:59:20 +0500

    added the files

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import *
+from .models import *
 # Create your views here.
+
+
+def home(request):
+    
+    home = Home.objects.latest('updated')
+    
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
+    
+    categories = Category.objects.all()
+    
+    portfolios = Portfolio.objects.all()
+    context = {
+        'home':home,
+        'about':about,
+        'profiles':profiles,
+        'categories':categories,
+        'portfolios':portfolios,
+    }
+    return render(request, 'portfolio/index.html', context)
